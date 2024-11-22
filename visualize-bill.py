34656e4812333edb30c6a819ae7c02ae16376a15
azazo1 <azazo1@qq.com>
2024-11-22 13:11:53 +0800

    visualize-bill 现在支持显示电量消耗速度.

--- visualize-bill.py
+++ visualize-bill.py
@@ -45,6 +45,29 @@ def load_data():
     return timestamp, degree
 
 
+def smooth(data, alpha=0.6):
+    if not data:
+        return []
+    r = data[0]
+    rst = []
+    for i in data:
+        r = r * alpha + i * (1 - alpha)
+        rst.append(r)
+    return rst
+
+
+def consuming_speed(timestamp, degree):
+    t, s = [], []  # 消耗速度时间戳和消耗速度, 单位: 度/天.
+    for i in range(len(timestamp)):
+        if i == len(timestamp) - 1:
+            break
+        delta_time = timestamp[i + 1] - timestamp[i]
+        t.append(delta_time / 2 + timestamp[i])
+        s.append((degree[i] - degree[i + 1]) / delta_time * 3600 * 24)
+    s = smooth(s)
+    return t, s
+
+
 def main():
     asyncio.run(download_data())
     timestamp, degree = load_data()
@@ -52,14 +75,23 @@ def main():
         print("no data")
         return
     start_date = datetime.fromtimestamp(timestamp[0])
-    timestamp = list(
+    day_stamp = list(
         map(lambda x: (x - start_date.timestamp()) / 3600 / 24, timestamp)
     )
-    plt.plot(timestamp, degree, marker='o')
-    plt.title(f'电量使用情况, 从 {start_date.strftime("%Y年%m月%d日%H时%M分%S秒")} 开始')
-    plt.xlabel("时间(天)")
-    plt.ylabel("电量(度)")
-    plt.grid(True)
+    fig, ax = plt.subplots()
+    ax.plot(day_stamp, degree, marker='o')
+    ax.set_title(f'电量使用情况, 从 {start_date.strftime("%Y年%m月%d日%H时%M分%S秒")} 开始')
+    ax.set_xlabel("时间(天)")
+    ax.set_ylabel("电量(度)")
+    ax.grid(True)
+
+    timestamp, speed = consuming_speed(timestamp, degree)
+    day_stamp = [(i - start_date.timestamp()) / 3600 / 24 for i in timestamp]
+    ax1 = ax.twinx()
+    ax1.plot(day_stamp, speed, 'r--')
+    ax1.set_ylim(0)
+    ax1.set_ylabel('电量消耗速度(度/天)')
+
     plt.show()
 
 
